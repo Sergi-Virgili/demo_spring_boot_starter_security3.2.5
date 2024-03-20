cf91af6d4c1406f6fb3c224ae4db99c2e35441a5
Sergi-Virgili <svirgilif@gmail.com>
2024-03-20 17:22:06 +0100

    feat: add a simple open endpoints

---
+++ src/main/java/com/f5/securitybasic/controllers/AuthController.java
@@ -0,0 +1,19 @@
+package com.f5.securitybasic.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+public class AuthController {
+
+    @GetMapping("/open")
+    public String openEndpoint() {
+        return "Hello in OPEN ENDPONT";
+    }
+
+    @GetMapping("/closed")
+    public String closedEndpoint() {
+        return "Hello ypu are in the Restricted Area";
+    }
+
+}

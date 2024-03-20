b877a185006b8d5a69c5dcddfedb0fab756330d4
Sergi-Virgili <svirgilif@gmail.com>
2024-03-20 18:37:25 +0100

    chore: add empty authentication manager

--- src/main/java/com/f5/securitybasic/config/SecurityConfig.java
+++ src/main/java/com/f5/securitybasic/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.f5.securitybasic.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -13,5 +15,10 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
